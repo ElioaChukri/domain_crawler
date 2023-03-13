480794ed17dedc9c50afb62979074a6f7c870ae2
Elio <elioachukri@pm.me>
2023-03-13 21:11:55 +0200

    Add documentation and cleanup code. Also rename main.py to test_script.py

--- helpers.py
+++ helpers.py
@@ -2,20 +2,34 @@ import json
 import os
 import re
 import requests
-from main import DOMAIN
+from test_script import DOMAIN
 
 
 def loadFiles():
+	"""
+	Loads the directories and subdomains from the files
+	"""
+
 	with open("input_files/dirs_dictionary.bat", "r") as f1:
 		dirs = f1.read().splitlines()
 
-	with open("input_files/subdomains_dictionary.bat") as f2:
+	with open("input_files/subdomains_dictionary.bat", "r") as f2:
 		subdomains = f2.read().splitlines()
 
 	return dirs, subdomains
 
 
 def writeFiles(valid_dirs, valid_subdomains, file_dict):
+
+	"""
+	Writes the valid directories, subdomains, and files to the output_files directory
+	If the directory output_files does not exist, it will be created
+	:param valid_dirs:
+	:param valid_subdomains:
+	:param file_dict:
+	:return: void
+	"""
+
 	directory = "output_files"
 	if not os.path.exists(directory):
 		os.makedirs(directory)
@@ -33,9 +47,17 @@ def writeFiles(valid_dirs, valid_subdomains, file_dict):
 
 
 def crawlDirs(dirs):
+
+	"""
+	Queries all the possible directories listed in the file given and returns
+	a list of directories that return a 202 status code
+	:param dirs: list
+	:return valid_dirs: list
+	"""
+
 	valid_dirs = []
 	for directory in dirs:
-		url = f"{DOMAIN}/{directory}"
+		url = f"https://{DOMAIN}/{directory}"
 		request = requests.get(url)
 		if request.status_code == 202:
 			valid_dirs.append(url)
@@ -45,10 +67,18 @@ def crawlDirs(dirs):
 
 
 def crawlDomain(subdomains):
+
+	"""
+	Queries all the possible subdomains listed in the file given and returns
+	a list of subdomains that return a 202 status code
+	:param subdomains: list
+	:return valid_subdomains: list
+	"""
+
 	valid_subdomains = []
 	file_dict = {}
 	for subdomain in subdomains:
-		url = f"{subdomain}.{DOMAIN}"
+		url = f"https://{subdomain}.{DOMAIN}"
 		request = requests.get(url)
 		if request.status_code == 202:
 			valid_subdomains.append(url)
@@ -60,6 +90,15 @@ def crawlDomain(subdomains):
 
 
 def getFiles(url, html):
+
+	"""
+	Scrapes the html for possible files that are being linked to in the html and stores them in a dictionary
+	of key:value pairs equivalent to url:file
+	:param url: str
+	:param html: str
+	:return file_dict: dict
+	"""
+
 	file_dict = {}
 	pattern = r'href="(.+?)"'
 	files = re.findall(pattern, html)
--- test_script.py
+++ test_script.py
@@ -1,3 +1,11 @@
+"""
+------------------------------------------------------------------------------------------------------------------------
+Author name: Elio Anthony Chukri
+Last edited: March 13, 2023
+Project Description: Simple Python script that makes use of multithreading to brute force directories and subdomains
+------------------------------------------------------------------------------------------------------------------------
+"""
+
 from helpers import *
 from multiprocessing import cpu_count
 from concurrent.futures import ThreadPoolExecutor
@@ -6,16 +14,17 @@ import sys
 # TODO: Implement logging for different HTML status codes (timeout, not found, etc..)
 
 if len(sys.argv) != 2:
-	sys.exit("Usage: python3 main.py <domain>")
+	sys.exit("Usage: python3 test_script.py <domain>")
 
 DOMAIN = sys.argv[1]
 
 
 def main():
+
 	# Initializing the lists and dictionary that we will use to store the valid directories, subdomains, and files
 	valid_dirs = []
 	valid_subdomains = []
-	file_dict = {}
+	file_dict = {}  # dict because each url can have multiple files
 
 	# Loading the directories and subdomains from the files
 	dirs, subdomains = loadFiles()
@@ -27,7 +36,7 @@ def main():
 	divided_dirs = [dirs[i::max_processes] for i in range(max_processes)]
 	divided_subdomains = [subdomains[i::max_processes] for i in range(max_processes)]
 
-	# Multithreading the crawling of the directories and subdomains
+	# Using the ThreadPoolExecutor to create a pool of threads that will run the crawlDirs and crawlDomain functions
 	with ThreadPoolExecutor(max_workers=max_processes) as executor:
 		dir_workers = [executor.submit(crawlDirs, divided_dir) for divided_dir in divided_dirs]
 		domain_workers = [executor.submit(crawlDomain, divided_subdomain) for divided_subdomain in divided_subdomains]
@@ -36,11 +45,14 @@ def main():
 		# and valid_subdomains lists and the file_dict dict
 		for worker in dir_workers:
 			valid_dirs.extend(worker.result())
+
 		for worker in domain_workers:
 			valid_subdomains, file_dict = worker.result()
 			valid_subdomains.extend(valid_subdomains)
 			file_dict.update(file_dict)
 
+	# Writing the valid directories, subdomains, and files to their respective files
 	writeFiles(valid_dirs, valid_subdomains, file_dict)
 
-
+if __name__ == "__main__":
+	main()
