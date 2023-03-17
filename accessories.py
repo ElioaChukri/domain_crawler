e047e2b3159efd9e68364df5f404499b02a55721
Elio <elioachukri@pm.me>
2023-03-17 19:33:53 +0200

    Add formatter class to display help message in a more readable format

--- accessories.py
+++ accessories.py
@@ -8,6 +8,59 @@ import argparse
 from multiprocessing import cpu_count
 
 
+# Class to customize the help message format
+class CustomHelpFormatter(argparse.HelpFormatter):
+	"""
+	Formats the help message to display the arguments in a more readable format
+	"""
+
+	def _format_action_invocation(self, action):
+		"""
+		Formats the arguments in the help message
+		Args:
+			action (argparse.Action): Action object containing the arguments
+		Returns:
+			Formatted string containing the arguments
+		"""
+		if not action.option_strings:
+			metavar, = self._metavar_formatter(action, action.dest)(1)
+			return metavar
+		else:
+			parts = []
+
+			# If the Optional doesn't take a value, format is `-s, --long`
+			if action.nargs == 0:
+				parts.extend(action.option_strings)
+
+			# If the Optional takes a value, format is `-s ARGS, --long ARGS`
+			else:
+				default = action.dest.upper()
+				args_string = self._format_args(action, default)
+				for option_string in action.option_strings:
+					parts.append(f'{option_string} {args_string}')
+
+			return ', '.join(parts)
+
+	def _format_action(self, action):
+		"""
+		Formats the help message for each argument
+		Args:
+			action (argparse.Action): Action object containing the arguments
+		Returns:
+			formatted_help (str): Formatted string containing the help message for each argument
+		"""
+		help_text = self._expand_help(action)
+		action_header = self._format_action_invocation(action)
+
+		# Left-justify action header strings with padding
+		action_header = action_header.ljust(self._action_max_length)
+
+		# Create the formatted help string without a line break between the action and help text
+		formatted_help = f'{action_header}  {help_text}\n'
+
+		return formatted_help
+
+
 def createLogger():
 	"""
 	Creates a logger object that can be used to log messages to a file and the console
@@ -40,25 +93,29 @@ def parseArguments():
 	threads = cpu_count() - 2 if cpu_count() > 4 else 1
 	parser = argparse.ArgumentParser(
 		description="Simple script to crawl a domain for subdomains and directories",
-		formatter_class=argparse.RawTextHelpFormatter
+		formatter_class=CustomHelpFormatter,
+		add_help=False
 	)
 
+	# Help argument
+	parser.add_argument("-h", "--help", action="help", help="Show this help message and exit")
+
 	# Required argument: domain
-	parser.add_argument("domain", help="Specify a domain\n\n")
+	parser.add_argument("domain", help="Specify a domain")
 
 	# Optional argument: output_dir
-	parser.add_argument("-o", "--output_dir", help="Specify the output directory\n\n", default="output_files")
+	parser.add_argument("-o", "--output_dir", help="Specify the output directory", default="output_files")
 
 	# Optional argument: threads
 	parser.add_argument("-t", "--threads",
 	                    type=int, default=threads,
-	                    help=f"Specify the number of threads to use, (default: {threads})\n\n")
+	                    help=f"Specify the number of threads to use, (default: {threads})")
 
 	# Optional argument: username
-	parser.add_argument("-u", "--username", help="Specify the username\n\n")
+	parser.add_argument("-u", "--username", help="Specify the username")
 
 	# Optional argument: password_file
-	parser.add_argument("-p", "--password_file", help="Specify the password file\n\n")
+	parser.add_argument("-p", "--password_file", help="Specify the password file")
 
 	# Store arguments inside args object and return it
 	arguments = parser.parse_args()
